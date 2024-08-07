fa0c830842f6dd6e462b6cf2359a5126bf671fbd
Bahtiyar89 <bahtiyar1989@gmail.com>
2024-08-07 13:47:36 +0300

    register upd

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -5,22 +5,24 @@ import { FormRow, Logo, SubmitBtn } from '../components';
 
 const Register = () => {
   return (
-    <Form method='post' className='form'>
-      <Logo />
-      <h4>Register</h4>
-      <FormRow type='text' name='name' />
-      <FormRow type='text' name='lastName' labelText='last name' />
-      <FormRow type='text' name='location' />
-      <FormRow type='email' name='email' />
-      <FormRow type='password' name='password' />
-      <SubmitBtn />
-      <p>
-        Already a member?
-        <Link to='/login' className='member-btn'>
-          Login
-        </Link>
-      </p>
-    </Form>
+    <Wrapper>
+      <Form method='post' className='form'>
+        <Logo />
+        <h4>Register</h4>
+        <FormRow type='text' name='name' />
+        <FormRow type='text' name='lastName' labelText='last name' />
+        <FormRow type='text' name='location' />
+        <FormRow type='email' name='email' />
+        <FormRow type='password' name='password' />
+        <SubmitBtn />
+        <p>
+          Already a member?
+          <Link to='/login' className='member-btn'>
+            Login
+          </Link>
+        </p>
+      </Form>
+    </Wrapper>
   );
 };
 
