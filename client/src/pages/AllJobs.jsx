62b7b4634e4f5953a386e0ae859c20ff69a98215
Bahtiyar89 <bahtiyar1989@gmail.com>
2024-08-15 21:19:36 +0300

    delete job added

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import { toast } from 'react-toastify';
+import { JobsContainer, SearchContainer } from '../components';
+import customFetch from '../utils/customFetch';
+import { useLoaderData } from 'react-router-dom';
+import { useContext, createContext } from 'react';
+import { useQuery } from '@tanstack/react-query';
 
-const AllJobs = () => {
-  return <div>AllJobs</div>;
+export const loader = async ({ request }) => {
+  try {
+    const { data } = await customFetch.get('/jobs');
+    return {
+      data,
+    };
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
 };
+
+const AllJobsContext = createContext();
+const AllJobs = () => {
+  const { data } = useLoaderData();
+  console.log('ddd:', data);
+
+  return (
+    <AllJobsContext.Provider value={{ data }}>
+      <JobsContainer />
+    </AllJobsContext.Provider>
+  );
+};
+
+export const useAllJobsContext = () => useContext(AllJobsContext);
 
 export default AllJobs;
