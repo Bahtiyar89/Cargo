f37c6540d4e86cf1fc7bd399cf7b94d4bea19864
Bahtiyar89 <bahtiyar1989@gmail.com>
2024-08-15 13:09:19 +0300

    added login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,39 @@
-import React, { Fragment } from 'react';
 import { Link, Form, redirect, useNavigate } from 'react-router-dom';
-import { FormRow, Logo, SubmitBtn } from '../components';
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { FormRow, Logo, SubmitBtn } from '../components';
+import customFetch from '../utils/customFetch';
+import { toast } from 'react-toastify';
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  try {
+    await customFetch.post('/auth/login', data);
+
+    toast.success('Login successful');
+    return redirect('/dashboard');
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
+};
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const loginDemoUser = async () => {
+    const data = {
+      email: 'test@test.com',
+      password: 'secret123',
+    };
+    try {
+      await customFetch.post('/auth/login', data);
+      toast.success('Take a test drive');
+      navigate('/dashboard');
+    } catch (error) {
+      toast.error(error?.response?.data?.msg);
+    }
+  };
   return (
     <Wrapper>
       <Form method='post' className='form'>
@@ -25,5 +55,4 @@ const Login = () => {
     </Wrapper>
   );
 };
-
 export default Login;
