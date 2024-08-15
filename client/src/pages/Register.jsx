eefc1b81ffae22e00fd5b87a2ca0a9c0880061a1
Bahtiyar89 <bahtiyar1989@gmail.com>
2024-08-15 12:49:47 +0300

    register done

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,22 @@
-import React, { Fragment } from 'react';
 import { Form, redirect, Link } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
 import { FormRow, Logo, SubmitBtn } from '../components';
+import customFetch from '../utils/customFetch';
+import { toast } from 'react-toastify';
 
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  try {
+    await customFetch.post('/auth/register', data);
+    toast.success('Registration successful');
+    return redirect('/login');
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+
+    return error;
+  }
+};
 const Register = () => {
   return (
     <Wrapper>
@@ -25,5 +39,4 @@ const Register = () => {
     </Wrapper>
   );
 };
-
 export default Register;
